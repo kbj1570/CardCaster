fda22dca7897caf1a9f2ecab1b1c29584f8d2009
kbj1570 <playstudy00@gmail.com>
2024-11-17 19:35:54 +0900

    test

--- Assets/Scripts/CardTargetingSystem.cs
+++ Assets/Scripts/CardTargetingSystem.cs
@@ -37,7 +37,7 @@ public class CardTargetingSystem : MonoBehaviour {
 
     public void Initialize() {
         LoadTargetingRules();
-        //This is Test
+        //my test
     }
 
     private void LoadTargetingRules() {
